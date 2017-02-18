7e1179eb408f9c8925fdd845d977be28184bb56f
William Yao <williamyao1994@gmail.com>
2017-02-17 21:50:25 -0800

    wip: added more structure to class and card, imported native-base

--- client/src/scenes/Swipe/Swipe.js
+++ client/src/scenes/Swipe/Swipe.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
-import {
-    Button,
-    Image,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'native-base';
 import SwipeCards from 'react-native-swipe-cards';
 
-let Card = React.createClass({
-    render() {
-        return (
-            <View style={[styles.card, { backgroundColor: this.props.backgroundColor }]}>
-                <Text>{this.props.text}</Text>
-            </View>
-        )
-    }
-})
+// TODO: remove and use url
+var miku = require('../../images/miku.jpg')
+var kishimoto = require('../../images/kishimoto.jpg')
+var minami = require('../../images/minami.jpg')
+var suika = require('../../images/suika.jpg')
+var shizenya = require('../../images/shizenya.jpg')
 
+// TODO: get list from props
 const Cards = [
-    { text: 'Tomato', backgroundColor: 'red' },
-    { text: 'Aubergine', backgroundColor: 'purple' },
-    { text: 'Courgette', backgroundColor: 'green' },
-    { text: 'Blueberry', backgroundColor: 'blue' },
-    { text: 'Umm...', backgroundColor: 'cyan' },
-    { text: 'orange', backgroundColor: 'orange' },
+    { restaurantName: 'Miku', image: miku, rating: '5' },
+    { restaurantName: 'Kishimoto Japanese Restaurant', image: kishimoto, rating: '5' },
+    { restaurantName: 'Minami', image: minami, rating: '4' },
+    { restaurantName: 'Suika', image: suika, rating: '4' },
+    { restaurantName: 'Shizen Ya', image: shizenya, rating: '4' },
 ]
 
 class Swipe extends Component {
@@ -39,16 +31,25 @@ class Swipe extends Component {
     static navigationOptions = {
         title: 'Swipe'
     };
-    
-    handleYup (card) {
-        console.log('Yup for ${card.text}');
+
+    Card(restaurant) {
+        return (
+            <View style={[styles.card]}>
+                <Image source={restaurant.image} resizeMode="contain" style={{ width: 350, height: 350 }} />
+                <Text>{restaurant.restaurantName}</Text>
+            </View>
+        )
     }
 
-    handleNope (card) {
-        console.log('Yup for ${card.text}');        
+    handleYup(card) {
+        console.log('Yup for ${card.restaurantName}');
     }
 
-    noMore(){
+    handleNope(card) {
+        console.log('Yup for ${card.restaurantName}');
+    }
+
+    noMore() {
         return (
             <Text>No more</Text>
         )
@@ -58,26 +59,35 @@ class Swipe extends Component {
         const { navigate } = this.props.navigation;
 
         return (
+            <View style={styles.container}>
                 <SwipeCards
                     cards={this.state.card}
 
-                    renderCard={(cardData) => <Card {...cardData} />}
+                    renderCard={(cardData) => this.Card(cardData)}
                     renderNoMoreCards={this.noMore}
 
                     handleYup={this.handleYup}
                     handleNope={this.handleNope}
                 />
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>test</Text>
+                </View>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f7f7f7'
+    },
     card: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        width: 300,
-        height: 300,
+        borderWidth: 5, 
+        borderColor: '#d6d7da',
     }
 })
 
