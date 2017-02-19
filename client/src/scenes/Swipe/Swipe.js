1eaef0dfcb3cd33680dd7558ebff84c9e5517f75
William Yao <williamyao1994@gmail.com>
2017-02-18 16:55:17 -0800

    wip: added buttons and changed layout a bit

--- client/src/scenes/Swipe/Swipe.js
+++ client/src/scenes/Swipe/Swipe.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'native-base';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Container, Icon } from 'native-base';
 import SwipeCards from 'react-native-swipe-cards';
 
 // TODO: remove and use url
@@ -12,11 +12,11 @@ var shizenya = require('../../images/shizenya.jpg')
 
 // TODO: get list from props
 const Cards = [
-    { restaurantName: 'Miku', image: miku, rating: '5' },
-    { restaurantName: 'Kishimoto Japanese Restaurant', image: kishimoto, rating: '5' },
-    { restaurantName: 'Minami', image: minami, rating: '4' },
-    { restaurantName: 'Suika', image: suika, rating: '4' },
-    { restaurantName: 'Shizen Ya', image: shizenya, rating: '4' },
+    { name: 'Miku', image: miku, rating: 5 },
+    { name: 'Kishimoto', image: kishimoto, rating: 5 },
+    { name: 'Minami', image: minami, rating: 4 },
+    { name: 'Suika', image: suika, rating: 4 },
+    { name: 'Shizen Ya', image: shizenya, rating: 4 },
 ]
 
 class Swipe extends Component {
@@ -26,6 +26,10 @@ class Swipe extends Component {
         this.state = {
             card: Cards
         };
+
+        this.onClickYup = this.onClickYup.bind(this);
+        this.onClickNope = this.onClickNope.bind(this);
+        this.getRating = this.getRating.bind(this);
     }
 
     static navigationOptions = {
@@ -35,18 +39,41 @@ class Swipe extends Component {
     Card(restaurant) {
         return (
             <View style={[styles.card]}>
-                <Image source={restaurant.image} resizeMode="contain" style={{ width: 350, height: 350 }} />
-                <Text>{restaurant.restaurantName}</Text>
+                <View style={{ flexDirection: 'row'}} >
+                    <Text style={{ fontSize: 40,  color: '#444' }}>{restaurant.name}</Text>
+                </View>
+                <Image source={restaurant.image} resizeMode="cover" style={{ width: 350, height: 350 }} />
+                {this.getRating(restaurant)}
             </View>
         )
     }
 
+    getRating(restaurant) {
+        var icons = []
+        for (var i = 0; i < restaurant.rating; i++) {
+            icons.push(<Icon key={restaurant.name + i} name='star'></Icon>);
+        }
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                {icons}
+            </View>
+        );
+    }
+
     handleYup(card) {
-        console.log('Yup for ${card.restaurantName}');
+        console.log('Yup for ${card.name}');
+    }
+
+    onClickYup() {
+        this.swiper._goToNextCard();
     }
 
     handleNope(card) {
         console.log('Yup for ${card.restaurantName}');
+    }
+
+    onClickNope() {
+        this.swiper._goToNextCard();        
     }
 
     noMore() {
@@ -61,16 +88,21 @@ class Swipe extends Component {
         return (
             <View style={styles.container}>
                 <SwipeCards
+                    ref={(card) => { this.swiper = card; }}
                     cards={this.state.card}
 
                     renderCard={(cardData) => this.Card(cardData)}
                     renderNoMoreCards={this.noMore}
-
                     handleYup={this.handleYup}
                     handleNope={this.handleNope}
                 />
-                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>test</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 5 }}>
+                    <TouchableOpacity style={styles.button} onPress={() => this.onClickNope()}>
+                        <Icon name='close' size={45} color="#888" />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={() => this.onClickYup()}>
+                        <Icon name='heart' size={36} color="#888" />
+                    </TouchableOpacity>
                 </View>
             </View>
         );
@@ -78,6 +110,15 @@ class Swipe extends Component {
 }
 
 const styles = StyleSheet.create({
+    button: {
+        width: 80,
+        height: 80,
+        borderWidth: 10,
+        borderColor: '#e7e7e7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 40
+    },
     container: {
         flex: 1,
         backgroundColor: '#f7f7f7'
@@ -86,7 +127,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        borderWidth: 5, 
+        borderWidth: 5,
         borderColor: '#d6d7da',
     }
 })
