145284047564da2e3f511a4dd00126eb5c00e2d5
Edmund Wong <w.edmund.hh@gmail.com>
2017-02-20 18:14:23 -0800

    Add clickable input options to preferences view

--- client/src/scenes/Events/Preferences/Preferences.js
+++ client/src/scenes/Events/Preferences/Preferences.js
@@ -7,7 +7,12 @@ import {
   Button
 } from 'react-native';
 
+import PopupModal from '../../../components/PopupModal';
 import SettingsBtn from '../../../components/SettingsBtn';
+import SelectList from '../../../components/SelectList';
+
+const priceOptions = [5, 15, 25, 40, 60, 100];
+const distanceOptions = [1, 2, 3, 5, 10, 20];
 
 class Preferences extends Component {
   static propTypes = {
@@ -20,45 +25,103 @@ class Preferences extends Component {
     title: 'Restaurant Preferences'
   }
 
-   constructor(props) {
+  constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(this.props.preferences.cuisineTypes),
-
+      modalVisible: false,
+      modalOptions: {
+        options: [],
+        onSelect: () => {}
+      },
+      dataSource: ds.cloneWithRows([]),
+      preferences: this.props.preferences
     };
   }
 
-  changeDistance = () => {
-    this.props.changeRadius(this.props.preferences.distance + 1);
+  openModal = (options) => {
+    return () => {
+      this.setState({
+        modalVisible: true,
+        modalOptions: {
+          options: options.options,
+          onSelect: options.onSelect
+        }
+      });
+    }
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalVisible: false
+    });
+  };
+
+  changeDistance = (value) => {
+    this.setState({
+      modalVisible: false,
+      preferences: {
+        ...this.state.preferences,
+        distance: value
+      }
+    });
+  };
+
+  changeMinPrice = (value) => {
+    this.setState({
+      modalVisible: false,
+      preferences: {
+        ...this.state.preferences,
+        minPrice: value
+      }
+    });
+  };
+
+  changeMaxPrice = (value) => {
+    this.setState({
+      modalVisible: false,
+      preferences: {
+        ...this.state.preferences,
+        maxPrice: value
+      }
+    });
   };
 
   render() {
     return (
       <View>
         <SettingsBtn 
-            onPress={this.changeDistance}
+            onPress={this.openModal({options: distanceOptions, onSelect: this.changeDistance})}
             label="Maximum Travel Distance"
-            value={this.props.preferences.distance} />
+            value={this.state.preferences.distance} />
         <SettingsBtn 
-            onPress={this.changeDistance}
+            onPress={this.openModal({options: priceOptions, onSelect: this.changeMinPrice})}
             label="Minimum Price"
-            value={this.props.preferences.minPrice} />
+            value={this.state.preferences.minPrice} />
         <SettingsBtn 
-            onPress={this.changeDistance}
+            onPress={this.openModal({options: priceOptions, onSelect: this.changeMaxPrice})}
             label="Maximum Price"
-            value={this.props.preferences.maxPrice} />
+            value={this.state.preferences.maxPrice} />
         <SettingsBtn 
             onPress={this.changeDistance}
             label="Cuisine Type" />
+        <ListView
+            dataSource={this.state.dataSource}
+            enableEmptySections={true}
+            renderRow={(rowData, sectionId) => {return (<Text>{rowData}</Text>);}} />
+
+        <PopupModal 
+            visible={this.state.modalVisible}
+            onClose={this.closeModal}>
+          <SelectList
+              options={this.state.modalOptions.options}
+              onSelect={this.state.modalOptions.onSelect} />
+
+        </PopupModal>
 
         <Button
            onPress={() => this.props.savePreferences(1, {})}
            title="DONE!" />
-        <ListView
-            dataSource={this.state.dataSource}
-            enableEmptySections={true}
-            renderRow={(rowData, sectionId) => {console.log(sectionId); return (<Text>{rowData}</Text>);}} />
       </View>
     );
   }
