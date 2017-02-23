e16e28d2fa27a05ec3ecfb60d74b35ddfb8e1189
Edmund Wong <w.edmund.hh@gmail.com>
2017-02-22 20:55:43 -0800

    Create component for creating list of cuisine types which can be removed

---
+++ client/src/components/RemovableItemsList/RemovableItemsList.js
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes } from 'react';
+import { List, ListItem, Body, Right, Button, Icon } from 'native-base';
+
+class RemovableItemsList extends Component {
+  static propTypes = {
+    list: PropTypes.array.isRequired,
+    renderRow: PropTypes.func.isRequired,
+    onRemove: PropTypes.func.isRequired
+  };
+
+  render() {
+    return (
+      <List
+          dataArray={this.props.list}
+          renderRow={(rowData) => 
+        <ListItem>
+          <Body>
+            {this.props.renderRow(rowData)}
+          </Body>
+          <Right>
+            <Button 
+                transparent
+                onPress={() => this.props.onRemove(rowData)}>
+              <Icon name='md-close' />
+            </Button>
+          </Right>
+        </ListItem>
+      } />
+    );
+  }
+
+}
+
+export default RemovableItemsList;
