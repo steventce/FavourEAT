112adb01e9f454da0ba38aa6c95f18b1c67a7444
William Yao <williamyao1994@gmail.com>
2017-02-24 21:50:25 -0800

    date time picker

--- client/src/scenes/Events/CreateEvent/CreateEvent.js
+++ client/src/scenes/Events/CreateEvent/CreateEvent.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Text, Image, View } from 'react-native';
-import { Container, Content, Form, H1, Item, Input } from 'native-base';
+import { Button, Container, Content, Form, H1, Item, Input, Label } from 'native-base';
+import DatePicker from 'react-native-datepicker';
 
 class CreateEvent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: new Date(),
+      time: new Date()
+    };
+  }
 
   render() {
     return (
@@ -12,11 +21,50 @@ class CreateEvent extends Component {
             <H1>CreateEvent</H1>
           </View>
           <Form>
-            <Item>
-              <Text>Event Name</Text>
+            <Item fixedLabel>
+              <Label>Event Name: </Label>
               <Input />
             </Item>
+            <Item fixedLabel>
+              <DatePicker
+                date={this.state.date}
+                mode="date"
+                format="YYYY-MM-DD"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                minDate={new Date()}
+                showIcon={false}
+                onDateChange={(date) => {this.setState({ date: date })}} />
+                <DatePicker
+                date={this.state.time}
+                mode="time"
+                format="HH:mm"
+                confirmBtnText="Confirm"
+                cancelBtnText="Cancel"
+                showIcon={false}
+                onDateChange={(time) => {this.setState({ time: time })}} />
+            </Item>
+            <Item fixedLabel>
+              <Label>Cuisine: </Label>
+              <Input disabled />
+            </Item>
+            <Item fixedLabel>
+              <Label>Price Range: </Label>
+              <Input disabled />
+            </Item>
+            <Item fixedLabel last>
+              <Label>Max Distance: </Label>
+              <Input disabled />
+            </Item>
           </Form>
+          <View>
+          <Button primary>
+            <Text>Set Preferences</Text>
+          </Button>
+          <Button primary>
+            <Text>Create Event</Text>
+          </Button>
+          </View>
         </Content>
       </Container>
     );
