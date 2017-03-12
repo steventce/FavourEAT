36fcacef2a06fb356ddb83ad753351c44d173f95
Steven Eng <steven_eng@live.ca>
2017-03-11 20:54:07 -0800

    Refactor create events + test

--- server/server/favoureat/tests.py
+++ server/server/favoureat/tests.py
@@ -1,3 +1,4 @@
+from mock import patch
 from django.utils import timezone
 from django.core.management import call_command
 from django.test import TestCase
@@ -70,8 +71,6 @@ class UserSwipeTests(APITestCase):
         """ Ensure we can save a user swipe decision """
         user = User.objects.create(pk='139530')
         user.save()
-        print "users"
-        print User.objects.values_list('id', flat=True)
 
         data = {'user': user.id,
                 'yelp_id': 'yelp_business',
@@ -139,13 +138,31 @@ class ClearRestaurantCacheTests(TestCase):
         self.assertEqual(None, Restaurant.objects.get().json)
 
 class EventTests(APITestCase):
-    def test_get_user_events(self):
-        """ Ensures that a user can successfully retrieve their events """
-        expected_len = 5
-        user_id = 10
+    data = {
+        'radius': 100, 'latitude': 49, 'longitude': -123, 'min_price': 10,
+        'max_price': 50, 'name': 'Test'
+    }
+
+    def event_helper(self, user_id, data, authenticate, method='GET'):
+        """ Helper to post to the create event endpoint """
         user = User(pk=user_id)
         user.save()
 
+        url = 'v1/users/{user_id}/events'.format(user_id=str(user_id))
+        factory = APIRequestFactory()
+        request = factory.post(url, data, format='json') if method == 'POST' else factory.get(url)
+
+        if authenticate:
+            force_authenticate(request, user)
+        view = views.EventView.as_view()
+        response = view(request, user_id=user_id)
+        return response
+
+    def test_get_user_events(self):
+        """ Ensures that a user can successfully retrieve their events """
+        expected_len = 5
+        user = User(pk=10)
+        user.save()
         for index in xrange(expected_len):
             preference = Preference(radius=1, latitude=2, longitude=3)
             preference.save()
@@ -159,14 +176,81 @@ class EventTests(APITestCase):
 
             event = Event(creator=user, event_detail=event_detail, round_num=0)
             event.save()
-
             user.eventuserattach_set.create(event=event, user=user)
 
-        factory = APIRequestFactory()
-        request = factory.get('v1/users/{user_id}/events'.join([str(user_id)]))
-        force_authenticate(request, user=user)
-        view = views.EventView.as_view()
-        response = view(request, user_id=user_id)
+        response = self.event_helper(10, None, True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), expected_len)
+
+    def test_event_save_no_token(self):
+        """ Ensure that an unauthorized error occurs with no authentication """
+        response = self.event_helper(10, None, False, 'POST')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_event_save_no_params(self):
+        """ Ensure that a 400 response is returned when there are no params """
+        response = self.event_helper(10, {}, True, 'POST')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @patch('server.favoureat.recommendation_service.RecommendationService.get_restaurants')
+    def test_event_save_pref(self, get_restaurants_mock):
+        """ Ensure that preferences are saved correctly when creating an event """
+        user_id = 10
+        # Mock out the Yelp API call
+        get_restaurants_mock.return_value = []
+
+        response = self.event_helper(user_id, self.data, True, 'POST')
+        event = Event.objects.get(creator=user_id)
+        self.assertEqual(event.event_detail.name, self.data['name'])
+
+        preference = event.event_detail.preference
+
+        self.assertEqual(preference.radius, self.data['radius'])
+        self.assertEqual(preference.latitude, self.data['latitude'])
+        self.assertEqual(preference.longitude, self.data['longitude'])
+        self.assertEqual(preference.min_price, self.data['min_price'])
+        self.assertEqual(preference.max_price, self.data['max_price'])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @patch('server.favoureat.recommendation_service.RecommendationService.get_restaurants')
+    def test_event_save_detail(self, get_restaurants_mock):
+        """ Ensure that event details are saved correctly when creating an event """
+        user_id = 10
+        # Mock out the Yelp API call
+        get_restaurants_mock.return_value = []
+
+        response = self.event_helper(user_id, self.data, True, 'POST')
+        event = Event.objects.get(creator=user_id)
+        self.assertEqual(event.event_detail.name, self.data['name'])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @patch('server.favoureat.recommendation_service.RecommendationService.get_restaurants')
+    def test_event_save_attach(self, get_restaurants_mock):
+        """ Ensure that event user attach is saved correctly when creating an event """
+        user_id = 10
+        # Mock out the Yelp API call
+        get_restaurants_mock.return_value = []
+
+        response = self.event_helper(user_id, self.data, True, 'POST')
+        event = Event.objects.get(creator=user_id)
+        self.assertEqual(event.eventuserattach_set.first().user_id, user_id)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @patch('server.favoureat.recommendation_service.RecommendationService.get_restaurants')
+    def test_event_save_tournament(self, get_restaurants_mock):
+        """ Ensure that tournaments are saved correctly when creating an event """
+        user_id = 10
+        restaurants = Restaurant.objects.bulk_create([
+            Restaurant(yelp_id='Cheap Foods'),
+            Restaurant(yelp_id='Some Pizza'),
+            Restaurant(yelp_id='Good Rice')
+        ])
+        get_restaurants_mock.return_value = restaurants
+
+        response = self.event_helper(user_id, self.data, True, 'POST')
+        event = Event.objects.get(creator=user_id)
+        self.assertEqual(len(event.tournament_set.values()), len(restaurants))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
