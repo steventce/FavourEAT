c1e72a31827a89cf2c952f79cfcfd81fef7ba9a9
Edmund Wong <w.edmund.hh@gmail.com>
2017-03-15 13:46:45 -0700

    Get current location and use it with restaurant coordinates to set initial position of map

--- client/src/scenes/Map/Map.js
+++ client/src/scenes/Map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, PermissionsAndroid } from 'react-native';
 import { Container, Content } from 'native-base';
 import MapView from 'react-native-maps';
 
@@ -8,14 +8,53 @@ import styles from './styles';
 class Map extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+      position: {}
     }
+  }
+
+  setUpWatchPosition = () => {
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      this.setState({position: { latitude, longitude }});
+    });
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const { restaurant } = this.props.navigation.state.params;
+        const rLatitude = restaurant.coordinates.latitude;
+        const rLongitude = restaurant.coordinates.longitude;
+        console.log(restaurant)
+
+        const region = {
+          latitude: (rLatitude + latitude) / 2,
+          longitude: (rLongitude + longitude) / 2,
+          latitudeDelta: Math.abs(rLatitude - latitude),
+          longitudeDelta: Math.abs(rLongitude - longitude),
+        }
+
+        this.setState({
+          region: region,
+          position: {latitude, longitude}
+        }, this.setUpWatchPosition);
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
   }
 
   onRegionChange = (region) => {
@@ -23,11 +62,8 @@ class Map extends Component {
   }
 
   render() {
-     const screen = Dimensions.get('window');
-     const screenSize = {
-      width: screen.width,
-      height: screen.height,
-     };
+    console.log("region", this.state.region);
+    console.log("position", this.state.position);
 
     return (
       <MapView
