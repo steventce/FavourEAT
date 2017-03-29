6c90d365e6d109f6b581c5c10057c1b686e716b7
Steven Eng <steven_eng@live.ca>
2017-03-28 23:35:15 -0700

    Register notifications 1hr, 1day before events on home page

--- client/src/scenes/Events/UserEvents/UserEventsContainer.js
+++ client/src/scenes/Events/UserEvents/UserEventsContainer.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import { fetchEvents } from '../../../reducers/Events/actions';
 import UserEvents from './UserEvents';
+import { scheduleNotifs } from '../../../services/fcmService';
 
 const mapStateToProps = function(state) {
   const { auth, event } = state;
-  return { auth, events: event.events, status: event.status };
+  scheduleNotifs(event.events);
+  return {
+    auth,
+    events: event.events,
+    status: event.status
+  };
 };
 
 const mapDispatchToProps = function(dispatch) {
