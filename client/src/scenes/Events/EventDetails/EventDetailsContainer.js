778553f3aa0f0e58c3f8c2f8001b2178b68e57c8
Steven Eng <steven_eng@live.ca>
2017-03-29 17:51:50 -0700

    Refactor + validate edit event details

--- client/src/scenes/Events/EventDetails/EventDetailsContainer.js
+++ client/src/scenes/Events/EventDetails/EventDetailsContainer.js
@@ -2,6 +2,16 @@ import { connect } from 'react-redux';
 import EventDetails from './EventDetails';
 import { editEventDetails, cancelEvent } from '../../../reducers/Events/actions';
 import { getRound } from '../../../reducers/Tournament/actions';
+import {
+  validateEventName,
+  validateEventDatetime
+} from '../../../utils/common';
+import moment from 'moment';
+
+const validateEditEvent = function(eventDetails) {
+  const { name, datetime } = eventDetails
+  return validateEventName(name) && validateEventDatetime(moment(datetime));
+}
 
 const mapStateToProps = function(state, props) {
   const { navigation } = props;
@@ -18,6 +28,7 @@ const mapStateToProps = function(state, props) {
 const mapDispatchToProps = function(dispatch) {
   return {
     editEventDetails: (accessToken, userId, eventId, eventDetails) => {
+      if (!validateEditEvent(eventDetails)) return;
       dispatch(editEventDetails(accessToken, userId, eventId, eventDetails));
     },
     cancelEvent: (accessToken, userId, eventId) => {
--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -1,5 +1,29 @@
+import { Alert } from 'react-native';
 import moment from 'moment';
 
 export function isUpcoming(event) {
   return moment(event.event_detail.datetime).isSameOrAfter(moment(new Date()));
+}
+
+// True == successful validation
+export function validateEventName(name) {
+  if (!name) {
+    Alert.alert('Error', 'Please enter an event name.')
+  }
+  return !!name;
+}
+
+export function validateEventDatetime(eventMoment) {
+  if (!eventMoment.isValid() || moment().isAfter(eventMoment)) {
+    Alert.alert('Error', 'Invalid date & time entered.')
+    return false;
+  }
+  return true;
+}
+
+export function validateRndDuration(rndDuration) {
+  if (!rndDuration) {
+    Alert.alert('Error', 'Please select an option for round duration.')
+  }
+  return !!rndDuration;
 }
