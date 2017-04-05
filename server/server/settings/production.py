ae1895d42abd1fda12db1b68a694a0af49fafead
ncboodah <n.cheng@alumni.ubc.ca>
2017-04-04 20:13:26 -0700

    move config stuff for celery with heroku

--- server/server/settings/production.py
+++ server/server/settings/production.py
@@ -10,9 +10,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-  CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-
 SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
 SOCIAL_AUTH_CLIENT_ID = os.environ.get('SOCIAL_AUTH_CLIENT_ID')
