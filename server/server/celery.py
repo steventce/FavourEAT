1344c50e766ee99e2fa28b48d6a645f55a4db904
ncboodah <n.cheng@alumni.ubc.ca>
2017-04-05 22:36:13 -0700

    potential fix for redis on prod

--- server/server/celery.py
+++ server/server/celery.py
@@ -7,10 +7,14 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
-app = Celery('server', backend='redis://localhost:6379/0', broker='redis://localhost:6379/0')
 if os.environ.get('ENV') == 'PROD':
-	app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-		CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+	app = Celery('server', backend=os.environ['REDIS_URL'], broker=os.environ['REDIS_URL'])
+	# app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+	# 	CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+else:
+	app = Celery('server', backend='redis://localhost:6379/0', broker='redis://localhost:6379/0')
+
+
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
